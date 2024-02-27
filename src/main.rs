9e72b5d63e350d04f9244d385811fd19c052d3e9
RoelVoordendag <rvoordendag@gmail.com>
2024-02-27 23:16:55 +0100

    Setup reading Database url from env file and cleanup code a little

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,38 @@ mod routes;
 use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
 use chrono::Utc;
 use entity::messages;
-use uuid::Uuid;
 use sea_orm::{ActiveModelTrait, Database, DatabaseConnection};
 use sea_orm::ActiveValue::Set;
+use std::env;
+use std::fmt::format;
+use dotenv::dotenv;
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-    let db: DatabaseConnection = Database::connect("postgres://messaging_api:messaging_api@messaging_api_db.docker/messaging_api").await.expect("Something went wrong");
+async fn insert_message() -> impl Responder {
+    let database_url = env::var("DATABASE_URL").expect("The database url is not set");
+
+    let db: DatabaseConnection = Database::connect(database_url).await.expect("We could not setup database connection");
     
     HttpResponse::Ok().body("Hey there!");
 
-    let messaage = messages::ActiveModel{
+    let message = messages::ActiveModel{
         body: Set("Hallo dit is mijn eerste bericht".to_owned()),
         date_time: Set(Utc::now().naive_utc()),
         ..Default::default()
     };
 
-    messaage.insert(&db).await.expect("TODO: panic message");
+    message.insert(&db).await.expect("Could not insert message");
 
     HttpResponse::Ok().body("We just created something")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let db: DatabaseConnection = Database::connect("postgres://messaging_api:messaging_api@messaging_api_db.docker/messaging_api").await.expect("Something went wrong");
+    dotenv().ok();
 
     HttpServer::new(|| {
         App::new()
@@ -39,7 +43,7 @@ async fn main() -> std::io::Result<()> {
                     .route("/hello", web::get().to(routes::routes::hello))
             )
             .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .route("/hey", web::get().to(insert_message))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
