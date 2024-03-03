0de83769ca483e5164d333469a792e37890ed1ef
RoelVoordendag <rvoordendag@gmail.com>
2024-03-03 22:42:46 +0100

    Created entity for rooms and users

--- entity/prelude.rs
+++ entity/prelude.rs
@@ -1,1 +1,3 @@
 pub use super::messages::Entity as Messages;
+pub use super::rooms::Entity as Rooms;
+pub use super::users::Entity as Users;
---
+++ migration/src/m20240303_211323_creation_users_rooms.rs
@@ -0,0 +1,69 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Users::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Users::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Users::Name).string().not_null())
+                    .col(ColumnDef::new(Users::CreatedAt).date_time().not_null())
+                    .to_owned(),
+            ).await.expect("Migration up in creating Users went wrong");
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Rooms::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Rooms::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Rooms::Name).string().not_null())
+                    .col(ColumnDef::new(Rooms::CreatedAt).date_time().not_null())
+                    .to_owned()
+        ).await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Users::Table).to_owned())
+            .await.expect("Could not remove Users table");
+
+        manager
+            .drop_table(Table::drop().table(Rooms::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Users {
+    Table,
+    Id,
+    Name,
+    CreatedAt,
+}
+
+#[derive(DeriveIden)]
+enum Rooms {
+    Table,
+    Id,
+    Name,
+    CreatedAt,
+}
